90c1f693166a4f5275c3dfaf45d28d55dd1c1ff8
Christian Greenfield <christiangreenfield@web.de>
2023-01-07 20:01:34 +0100

    solve switch in statusbar line 30

--- models/statusbar.class.js
+++ models/statusbar.class.js
@@ -28,21 +28,7 @@ class StatusBar extends DrawableObject {
 	}
 
 	resolveImageIndex() {
-		if (this.percentage == 100) {
-			return 5;
-		} else if (this.percentage > 80) {
-			return 4;
-		} else if (this.percentage > 60) {
-			return 3;
-		} else if (this.percentage > 40) {
-			return 2;
-		} else if (this.percentage > 20) {
-			return 2;
-		} else {
-			return 0;
-		}
-
-		/* switch (percentage) {
+		switch (true) {
 			case this.percentage == 100:
 				return 5;
 			case this.percentage > 80:
@@ -55,6 +41,6 @@ class StatusBar extends DrawableObject {
 				return 1;
 			default:
 				return 0;
-		} */
+		}
 	}
 }
